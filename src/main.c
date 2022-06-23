6f757496c02f9e222ee2d448c8e72294c86aaedd
João Victor <joaovictor.20739@gmail.com>
2022-06-23 11:28:37 -0400

    lista implementada na abb

--- src/main.c
+++ src/main.c
@@ -1,16 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct NO {
   int id;
+  char *nome;
 
-  struct NO* esq;
-  struct NO* dir;
+  struct NO *esq;
+  struct NO *dir;
 } NO;
+
+typedef struct list {
+  int id;
+  char *nome;
+
+  struct list *prox;
+}list;
 
 NO *raiz_insert = NULL;
 NO *raiz = NULL;
 int tamp_temp = 0;
+
+list *inicio = NULL;
+list *fim = NULL;
+int tam_list = 0;
+
+void add_list(int id, char *nome){
+  list *novo = (list*)malloc(sizeof(list));
+  novo->id = id;
+  novo->nome = nome;
+  novo->prox = NULL;
+  if(inicio == NULL){
+    inicio = novo;
+    fim = novo;
+  }else if(id <= inicio->id){
+    novo->prox = inicio;
+    inicio = novo;
+  }else if(id >= fim->id){
+    fim->prox = novo;
+    fim = novo;
+  }else{
+    list *aux = inicio;
+    while(aux->prox->id < novo->id){
+      aux = aux->prox;
+    }
+    novo->prox = aux->prox;
+    aux->prox = novo;
+  }
+}
+
+void imprimir(list *aux){
+  printf("%d ",aux->id);
+  printf("%s", aux->nome);
+  printf("\n");
+  if(aux->id == fim->id){
+    return;
+  }else{
+    imprimir(aux->prox);
+  }
+}
 
 NO* buscaRecursiva(NO *no, int id) {
   if (no == NULL)
@@ -24,9 +72,16 @@ NO* buscaRecursiva(NO *no, int id) {
 }
 
 NO *insere_na_arvore(NO *no, int id) {
+  list *aux = inicio;
   if (no == NULL){
     no = (NO *) malloc(sizeof(NO));
     no->id = id;
+    if(aux->id < id){
+      while (aux->id < id){
+        aux = aux->prox;
+      }  
+    }
+    no->nome = aux->nome;
     no->esq = NULL;
     no->dir = NULL;
     tamp_temp++;
@@ -90,6 +145,8 @@ void pos_ordem(NO *aux){
     }
     printf("ID: ");
     printf("%d\t", aux->id);
+    printf("Nome: ");
+    printf("%s\n", aux->nome);
 }
 void in_ordem(NO *aux){
     
@@ -97,16 +154,18 @@ void in_ordem(NO *aux){
         in_ordem(aux->esq);
     }
     printf("ID: ");
-    printf("%d", aux->id);
-    printf("\t");
+    printf("%d\t", aux->id);
+    printf("Nome: ");
+    printf("%s\n", aux->nome);
     if(aux->dir != NULL){
         in_ordem(aux->dir);
     }
 }
 void pre_ordem(NO *aux){
     printf("ID: ");
-    printf("%d", aux->id);
-    printf("\t");
+    printf("%d\t", aux->id);
+    printf("Nome: ");
+    printf("%s\n", aux->nome);
     if(aux->esq != NULL){
         pre_ordem(aux->esq);
     }
@@ -115,11 +174,30 @@ void pre_ordem(NO *aux){
     }
 }
 
-int main() {
-  int tam = 7;
-  int vetor_de_id[] = {1, 2, 3, 4, 5, 6, 7};
+void preenche_vec(int vetor_de_id[], int tam_vec){
+  list *aux = inicio;
 
-  arvore(raiz_insert,vetor_de_id,0,tam-1);
+  for(int i = 0; i < tam_vec; i++){
+    vetor_de_id[i] = aux->id;
+    aux = aux->prox;
+  }
+}
+
+int main() {
+
+  add_list(2, "jovi");
+  add_list(1, "pedro");
+  add_list(4, "billy");
+  add_list(3, "felp");
+  add_list(5, "wes");
+  imprimir(inicio);
+  printf("\n\n");
+  int tam_vec = fim->id;
+  int vetor_de_id[tam_vec];
+  preenche_vec(vetor_de_id, tam_vec);
+
+
+  arvore(raiz_insert,vetor_de_id,0,tam_vec-1);
   printf("Pos Ordem:\n");
   pos_ordem(raiz);
   printf("\n\n");
@@ -128,6 +206,7 @@ int main() {
   printf("\n\n");
   printf("Pre Ordem:\n");
   pre_ordem(raiz);
+
   abb_remove(raiz, 2);
   printf("\n\n");
   printf("Pos Ordem:\n");
