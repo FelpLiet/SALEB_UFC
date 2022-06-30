8cd1d3f7963387c54a3b79b6dc346b1ae62890f2
FelpLiet <felipe.leite23@gmai.com>
2022-06-30 17:19:31 -0300

    struct para pedidos

--- include/abb_encomendas.h
+++ include/abb_encomendas.h
@@ -3,24 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct NO {
-  int id;
+typedef struct NO
+{
+	int id;
 
-  struct NO* esq;
-  struct NO* dir;
+	struct NO *esq;
+	struct NO *dir;
 } NO;
 
+typedef struct no_encomenda
+{
+	char *nome_aluno;
+	int matricula_aluno;
+	char *tiulo_livro;
+	char *resumo_livo;
+	char *campus_livro;
+	char *campus_aluno;
+	char *responsavel_encomenda;
+	char *responsavel_trasporte;
+	int prioridade;
 
+	struct encomenda *dir;
+	struct encomenda *esq;
+}no_encomenda;
 
+// busca pelo vertice
+NO *buscaRecursiva(NO *no, int id);
 
-
-//busca pelo vertice
-NO* buscaRecursiva(NO *no, int id);
-
-//insere um vertice na arvore
+// insere um vertice na arvore
 NO *insere_na_arvore(NO *no, int id);
 
-NO *arvore( NO * raiz, NO *raiz_insert, int vetor_de_id[], int inicio, int fim);
+NO *arvore(NO *raiz, NO *raiz_insert, int vetor_de_id[], int inicio, int fim);
 
 void pos_ordem(NO *aux);
 
