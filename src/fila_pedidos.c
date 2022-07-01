cc7433fb7a49cccc650382c8c2a363027e4bbdd4
Billy <billygrahan09@gmail.com>
2022-07-01 12:35:36 -0300

    fila de pedidos atualizada

--- src/fila_pedidos.c
+++ src/fila_pedidos.c
@@ -20,50 +20,76 @@ void add_lista_pedidos(no_encomenda *pedido)
         fim = novo;
         tam++;
     }
-    else
-    { // add usando o antigo add fim da lista
+    else if (novo->prioridade < fim->prioridade)
+    {
         fim->prox = novo;
         fim = novo;
         tam++;
     }
+    else if (novo->prioridade > inicio->prioridade)
+    {
+        novo->prox = inicio;
+        inicio = novo;
+        tam++;
+    }
+    else
+    {
+        lista_pedidos *aux = inicio;
+        for (int i = 0; i < tam; i++)
+        {
+            if (aux->prox->prioridade < novo->prioridade)
+            {
+                novo->prox = aux->prox;
+                aux->prox = novo;
+                tam++;
+                break;
+            }
+        }
+    }
 }
 
-void remover_da_fila_de_prioridade(){
+void remover_da_fila_de_prioridade()
+{
     lista_pedidos removido;
-    if(inicio != NULL){
-        //remover usando o antigo remover do inicio da lista!
-            
-            lista_pedidos *lixo = inicio;
-            inicio = inicio->prox;
-            /*//copia os dados para o retorno!!_______________________________
-            removido.campus_aluno = lixo->campus_aluno;
-            removido.campus_livro = lixo->campus_livro;
-            removido.matricula_aluno = lixo->matricula_aluno;
-            removido.nome_aluno = lixo->nome_aluno;
-            removido.prioridade = lixo->prioridade;
-            removido.prox = NULL;
-            removido.responsavel_encomenda = lixo->responsavel_encomenda;
-            removido.responsavel_trasporte = lixo->responsavel_trasporte;
-            removido.resumo_livro = lixo->resumo_livro;
-            removido.titulo_livro = lixo->titulo_livro;
-            //______________________________________________________________*/
+    if (inicio != NULL)
+    {
+        // remover usando o antigo remover do inicio da lista!
 
-            free(lixo);
-            tam--;
-            if(tam == 1){
-                fim = NULL;
-            }   
+        lista_pedidos *lixo = inicio;
+        inicio = inicio->prox;
+        /*//copia os dados para o retorno!!_______________________________
+        removido.campus_aluno = lixo->campus_aluno;
+        removido.campus_livro = lixo->campus_livro;
+        removido.matricula_aluno = lixo->matricula_aluno;
+        removido.nome_aluno = lixo->nome_aluno;
+        removido.prioridade = lixo->prioridade;
+        removido.prox = NULL;
+        removido.responsavel_encomenda = lixo->responsavel_encomenda;
+        removido.responsavel_trasporte = lixo->responsavel_trasporte;
+        removido.resumo_livro = lixo->resumo_livro;
+        removido.titulo_livro = lixo->titulo_livro;
+        //______________________________________________________________*/
+
+        free(lixo);
+        tam--;
+        if (tam == 1)
+        {
+            fim = NULL;
+        }
     }
     printf("PEDIDO ENCAMINHADO!");
 }
 
-void ver_prioridade(){
+void ver_prioridade()
+{
+    if(inicio != NULL){
     puts("-----------------------------------------------------------------");
-    printf("titulo do livro: %s/n",inicio->titulo_livro);
-    printf("dados do livro: %s/n",inicio->resumo_livro);
-    printf("campus do livro: %s/n",inicio->campus_livro);
-    printf("destino do livro: %s/n",inicio->campus_aluno);
+    printf("titulo do livro: %s/n", inicio->titulo_livro);
+    printf("dados do livro: %s/n", inicio->resumo_livro);
+    printf("campus do livro: %s/n", inicio->campus_livro);
+    printf("destino do livro: %s/n", inicio->campus_aluno);
     puts("-----------------------------------------------------------------");
+    }
     printf("1 - PEDIDO ENCAMINHADO (REMOVER DA ESPERA)
             2 - EXIT /n/n");
     int N;
